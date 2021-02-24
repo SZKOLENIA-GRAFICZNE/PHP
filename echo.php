94236ab6cce6e764e178b057d4a99f6ff8ebf0c5
Paweł Zawadzki <38609429+machinacyfrowa@users.noreply.github.com>
2021-02-24 17:10:46 +0100

    polecenie echo

---
+++ echo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    Witaj świecie! <br>
+
+    <?php
+    //deklarujemy zmienną przechowująca tekst:
+    $powitanie = "Witaj świecie (PHP)! <br>";
+    //wyświetl zawartość zmiennej powitanie jako html
+    echo $powitanie;
+    //policz sumę pierwszych 5 cyfr i zapisz do zmiennej wynik
+    $wynik = 1 + 2 + 3 + 4 + 5;
+    //wyświetl na ekranie z informajcją:
+    echo "Suma pierwszych 5-ciu cyfr wynosi: ";
+    echo $wynik;
+    ?>
+</body>
+</html>
