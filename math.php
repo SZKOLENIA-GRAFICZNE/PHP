24f92180d4770a353fc0339bb149d44d0208896d
Paweł Zawadzki <38609429+machinacyfrowa@users.noreply.github.com>
2021-02-24 18:53:23 +0100

    funkcje matematyczne

--- math.php
+++ math.php
@@ -23,6 +23,16 @@
     $pole = round($pole, 2);
     //wypisujemy pole
     echo "Pole zadanego koła wynosi: $pole <br>";
+
+    //deklarujemy tabelę z ocenami
+    $oceny = array(3,2,4,5,1);
+    //używamy funkcji max aby pobrać najwyższą wartość
+    $najwyzszaOcena = max($oceny);
+    //wypisujemy
+    echo "najwyższa otrzymana ocena to: $najwyzszaOcena <br>";
+    //analogicznie działa funkcja min()
+
+    //innne przydatne: abs(), sqrt()
     ?>
 </body>
 </html>
