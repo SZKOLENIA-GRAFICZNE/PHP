fcf8c45cfbe692f88b1d352e8cac92505c927640
Paweł Zawadzki <38609429+machinacyfrowa@users.noreply.github.com>
2021-02-24 16:46:06 +0100

    podstawy składni

---
+++ podstawy.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+    </head>
+    <body>
+    <!-- komentrarz HTML -->
+    <?php 
+    // przed rozpoczęciem pisania skryptu musi być <?php
+
+    /* komentarz
+        wieloliniowy */
+
+    // przed powrotem do html musi być zakończenie jak niżej
+    ?> 
+
+    </body>
+</html>
